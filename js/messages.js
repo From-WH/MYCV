1af3454c2da1853b0192c50e8e6675688d05a0be
wanghang <mxw_sunshine@163.com>
2018-04-27 23:06:31 +0800

    添加留言板

--- js/messages.js
+++ js/messages.js
@@ -1,46 +1,90 @@
-var APP_ID = 'U42NTGFfgjD4gP4pmr1pBCaS-gzGzoHsz';
-var APP_KEY = 'X6BIXXme08ag0FKu3GrNsLec';
+!function () {
+    var APP_ID = 'U42NTGFfgjD4gP4pmr1pBCaS-gzGzoHsz';
+    var APP_KEY = 'X6BIXXme08ag0FKu3GrNsLec';
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-console.log('运行到这里显示')
+    AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+    });
 
 
-
-var query = new AV.Query('Message');
-query.find().then(function (messages) {
-    let array = messages.map((item) => item.attributes)
-    array.forEach((item) => {
-        let li = document.createElement('li')
-        li.innerText = item.content
-        let messageList = document.querySelector('#messageList')
-        messageList.append(li)
+    var query = new AV.Query('Message');
+    query.find().then(function (messages) {
+        let array = messages.map((item) => item.attributes)
+        array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.name}:${item.content}`
+            let messageList = document.querySelector('#messageList')
+            messageList.append(li)
+        })
     })
-})
 
 
 
+    let myForm = document.querySelector('#postMessageForm')
 
-let myForm = document.querySelector('#postMessageForm')
-
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let content = myForm.querySelector('input[name=content]').value
-    var Message = AV.Object.extend('Message');
-    var Message = new Message();
-    Message.save({
-        'content': content
-    }).then(function (object) {
-        alert('留言成功');
+    myForm.addEventListener('submit', function (e) {
+        e.preventDefault()
+        let content = myForm.querySelector('input[name=content]').value
+        let name = myForm.querySelector('input[name=name]').value
+        var Message = AV.Object.extend('Message');
+        var Message = new Message();
+        Message.save({
+            'content': content,
+            'name': name
+        }).then(function (object) {
+            let li = document.createElement('li')
+            li.innerText = `${object.attributes.name}:${object.attributes.content}`
+            let messageList = document.querySelector('#messageList')
+            messageList.append(li)
+            myForm.querySelector('input[name=content]').value = ''
+        })
     })
-})
+}.call()
 
-// var TestObject = AV.Object.extend('TestObject');
-// var testObject = new TestObject();
-// testObject.save({
-//     words: 'Hello World!'
-// }).then(function (object) {
-//     alert('LeanCloud Rocks!');
-// })
+
+
+// MVC前代码
+// !function () {
+//     var APP_ID = 'U42NTGFfgjD4gP4pmr1pBCaS-gzGzoHsz';
+//     var APP_KEY = 'X6BIXXme08ag0FKu3GrNsLec';
+
+//     AV.init({
+//         appId: APP_ID,
+//         appKey: APP_KEY
+//     });
+
+
+//     var query = new AV.Query('Message');
+//     query.find().then(function (messages) {
+//         let array = messages.map((item) => item.attributes)
+//         array.forEach((item) => {
+//             let li = document.createElement('li')
+//             li.innerText = `${item.name}:${item.content}`
+//             let messageList = document.querySelector('#messageList')
+//             messageList.append(li)
+//         })
+//     })
+
+
+
+//     let myForm = document.querySelector('#postMessageForm')
+
+//     myForm.addEventListener('submit', function (e) {
+//         e.preventDefault()
+//         let content = myForm.querySelector('input[name=content]').value
+//         let name = myForm.querySelector('input[name=name]').value
+//         var Message = AV.Object.extend('Message');
+//         var Message = new Message();
+//         Message.save({
+//             'content': content,
+//             'name': name
+//         }).then(function (object) {
+//             let li = document.createElement('li')
+//             li.innerText = `${object.attributes.name}:${object.attributes.content}`
+//             let messageList = document.querySelector('#messageList')
+//             messageList.append(li)
+//             myForm.querySelector('input[name=content]').value = ''
+//         })
+//     })
+// }.call()
